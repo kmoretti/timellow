c313ddf610f64a69e154fbcd589b22064a05c77f
克喵Moretti <kemiaosw@qq.com>
2026-06-27 14:38:30 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-define('TIMELLOW_THEME_VERSION', '1.0.12');
+define('TIMELLOW_THEME_VERSION', '1.0.11');
 define('TIMELLOW_DB_VERSION', '1.0.0');
 define('TIMELLOW_THEME_UPDATE_REPO', 'jkjoy/timellow');
 define('TIMELLOW_THEME_UPDATE_CACHE_KEY', 'timellow_theme_update_release');
